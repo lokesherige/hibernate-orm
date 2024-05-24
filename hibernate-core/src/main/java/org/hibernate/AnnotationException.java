37b15304158cdf918d56353edef0971efdad5d2f
lokesherige <lokesherige@gmail.com>
2024-05-24 16:00:06 +0530

    made comment

--- hibernate-core/src/main/java/org/hibernate/AnnotationException.java
+++ hibernate-core/src/main/java/org/hibernate/AnnotationException.java
@@ -5,6 +5,7 @@
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 package org.hibernate;
+//hello i am commiting one line
 
 /**
  * An exception that occurs while reading mapping annotations.
